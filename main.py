2abd21f9e3d2e1cce8f53f4ede8500b407ba764e
Harshith Kumar <123058145+Spyder67@users.noreply.github.com>
2024-06-19 13:20:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def progress(current, total, message, type):
 @bot.on_message(filters.command(["start"]))
 def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
 	bot.send_message(message.chat.id, f"**__♻️ Hi** **{message.from_user.mention}**, **I am Save Restricted Bot, I can send you restricted content by it's post link__**\n\n{USAGE}",
-	reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("☢️ Bot Developer", url="https://t.me/SUMAN")]]), reply_to_message_id=message.id)
+	reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("☢️ Bot Developer", url="tg://openmessage?user_id=1069420718")]]), reply_to_message_id=message.id)
 
 
 @bot.on_message(filters.text)
